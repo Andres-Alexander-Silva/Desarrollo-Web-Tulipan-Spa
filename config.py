6867e41e8e0b0ac3d68e459392b0a39bfbef8d29
Andres-Alexander-Silva <andresalexanderss@ufps.edu.co>
2022-06-06 22:01:30 -0500

    quinto commit

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    SECRET_KEY = "myScretKeyTulipanSpaAndresSilva"
-    
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:andres2611@localhost:5432/TulipanSpa"
